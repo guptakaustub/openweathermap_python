363a75da4296a38db7566963d2ad71e16b80300f
Prateek Jha <prateekj777@gmail.com>
2017-02-11 13:35:00 +0530

    Delete weather.py

--- weather.py
+++
@@ -1,9 +0,0 @@
-from pprint import pprint
-import requests
-
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Chennai&APPID=ffb1054510c074df76b0d02f3dd0fde4')
-#pprint(r.json())
-th = r.json()
-cur_w = th['main']['temp']
-cur_w = cur_w - 273
-print cur_w
